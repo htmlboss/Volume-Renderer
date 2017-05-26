7b20fe1a1cf05aafdcc9049441c0ca90b242ac53
Nabil <nmiri127@gmail.com>
2017-05-26 15:46:08 -0230

    Implmented Snapshot feature.
    * The current OGLWidget view can now be snapshotted and saved into a
    user-defined folder with an auto-incrementing filename.

--- ModelViewer/mainwindow.cpp
+++ ModelViewer/mainwindow.cpp
@@ -2,10 +2,15 @@
 #include "ui_mainwindow.h"
 
 #include <QFile>
+#include <QFileDialog>
 #include <QDebug>
+#include <QTime>
 
+/***********************************************************************************/
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
 	ui->setupUi(this);
+
+	ui->snapshotPushButton->setEnabled(false);
 
 	setWindowTitle("Viewer");
 
@@ -22,6 +27,38 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
 	statusBar()->showMessage("Ready");
 }
 
+/***********************************************************************************/
 MainWindow::~MainWindow() {
 	delete ui;
+}
+
+/***********************************************************************************/
+void MainWindow::on_actionAboutQt_triggered() {
+	QApplication::aboutQt();
+}
+
+/***********************************************************************************/
+void MainWindow::on_selectFolderPushButton_clicked() {
+	ui->screenshotFolder->setText(QFileDialog::getExistingDirectory(this, "Select a folder to save snapshots...", "", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks));
+}
+
+/***********************************************************************************/
+void MainWindow::on_snapshotPushButton_clicked() {
+	static uint_fast64_t snapshotNum;
+
+	const auto snapshot = ui->openGLWidget->grabFramebuffer();
+	const auto fileName = QString(ui->screenshotFolder->text() + "/snapshot" + QString::number(snapshotNum++) + ".png");
+
+	snapshot.save(fileName);
+
+	statusBar()->showMessage("Saved screenshot to: " + fileName + ".", 3000);
+}
+
+/***********************************************************************************/
+void MainWindow::on_screenshotFolder_textChanged(const QString& arg1) {
+	if (arg1.isEmpty()) {
+		ui->snapshotPushButton->setEnabled(false);
+		return;
+	}
+	ui->snapshotPushButton->setEnabled(true);
 }
--- ModelViewer/mainwindow.h
+++ ModelViewer/mainwindow.h
@@ -14,6 +14,15 @@ public:
 	explicit MainWindow(QWidget* parent = nullptr);
 	~MainWindow();
 
+private slots:
+	void on_actionAboutQt_triggered();
+
+	void on_selectFolderPushButton_clicked();
+
+	void on_snapshotPushButton_clicked();
+
+	void on_screenshotFolder_textChanged(const QString &arg1);
+
 private:
 	Ui::MainWindow* ui;
 };
